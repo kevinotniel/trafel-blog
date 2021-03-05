f7061110084058dd3dd8e4d57ef1edaac911115a
DESKTOP-PL7NIVF\ACER <kevinotniel3@gmail.com>
2021-03-05 13:00:19 +0700

    sticky nav

---
+++ tripTrafel/resources/js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+   
+   $('.js--feature-section').waypoint(function(direction) {
+      if (direction == "down") {
+         $('nav').addClass('sticky')
+      } else {
+         $('nav').removeClass('sticky')
+      }
+   }, {
+      offset: '60px'
+   })
+
+}) 
